0ba8f128c3091de407a5ebc851c0b994a6f038cd
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-10-13 16:04:33 -0500

    make it requirable

--- bin/belch.js
+++ bin/belch.js
@@ -8,8 +8,4 @@ program
   .option('-s, --sound [type]', 'specify the sound to play')
   .parse(process.argv);
 
-if (program.sound) {
-  belch(program.sound);
-} else {
-  belch('belch');
-}
+belch(program.sound);
